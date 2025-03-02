70d6dab9f0e0a61285774c11eaed55492a97f056
Φ Bui <bilphui@gmail.com>
2025-03-02 02:32:18 -0500

    added the qtable for navigation to cart

--- Q_Learning_agent.py
+++ Q_Learning_agent.py
@@ -25,6 +25,7 @@ class QLAgent:
             "pick_basket": self.load_qtable("pick_basket"),
             "navigate_shelf": self.load_qtable("navigate_shelf"),
             "pick_place": self.load_qtable("pick_place"),
+            "navigate_cart_return": self.load_qtable("navigate_cart_return"),
             "return_cart": self.load_qtable("return_cart")
         }
 
